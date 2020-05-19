c398c331ca012cb89bf25e0e47d734713c390048
Danilo Rodrigues Gonçalves <danilo.rodrigues59@gmail.com>
2020-05-19 18:03:50 -0300

    Upando Worker
    Upando Worker

---
+++ UserWorker/UserWorker/Domain/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UserWorker.Domain
+{
+    public sealed class User
+    {
+        public long id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string cnpj { get; set; }
+        public string email { get; set; }
+        public string senha { get; set; }
+
+    }
+}
---
+++ UserWorker/UserWorker/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using UserWorker.Domain;
+
+namespace UserWorker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "userQueue",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    try
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body.ToArray());
+                        var user = System.Text.Json.JsonSerializer.Deserialize<User>(message);
+
+                        Console.WriteLine($"Usuario {user.firstName} foi cadastrado!");
+
+                        channel.BasicAck(ea.DeliveryTag, false);
+                    }
+                    catch(Exception ex)
+                    {
+                        channel.BasicNack(ea.DeliveryTag, false, true);
+                    }
+                };
+
+
+                channel.BasicConsume(queue: "userQueue",
+                                     autoAck: false,
+                                     consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
